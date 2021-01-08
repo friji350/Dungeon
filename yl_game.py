45d05de7db314d220f796ad87f3ab85d78c67247
friji <67202651+friji350@users.noreply.github.com>
2021-01-08 17:15:15 +0300

    New Version
    Добавлен двойной прыжок
    Добавлена кнопка "restart" при смерти

--- yl_game.py
+++ yl_game.py
@@ -20,6 +20,8 @@ pygame.display.set_caption('Platformer')
 #загрузка заднего фона
 bg_img = pygame.image.load('img/background.jpg')
 
+restart_img = pygame.image.load('img/start.png')
+
 
 class Player():
 	def __init__(self, x, y):
@@ -27,18 +29,19 @@ class Player():
 		self.hero_r = []
 		self.hero_l = []
 		#загрузка тайтлов анимации
-		for i in range(1, 5):
+		for i in range(1, 8):
 			#оригинал
-			hero_r = pygame.image.load(f'img/guy{i}.png')
+			hero_l = pygame.image.load(f'img/slime{i}.png')
 			#изменение размеров
-			hero_r = pygame.transform.scale(hero_r, (100, 100))
+			hero_l = pygame.transform.scale(hero_l, (100, 77))
 			#отражаю 
-			hero_l = pygame.transform.flip(hero_r, True, False)
+			hero_r = pygame.transform.flip(hero_l, True, False)
 			self.hero_r.append(hero_r)
 			self.hero_l.append(hero_l)
 		#кадр в анимации (0 - стоит)
 		self.index = 0
 		self.coun = 0
+		self.jump_num = 0
 		
 		self.image = self.hero_r[self.index]
 		self.rect = self.image.get_rect()
@@ -56,9 +59,11 @@ class Player():
 		#обработка нажатия клавишь
 		if dead != 1:
 			key = pygame.key.get_pressed()
-			if key[pygame.K_SPACE] and self.jumped == False:
+			if key[pygame.K_SPACE] and self.jumped == False and self.jump_num < 2:
+				
 				self.vel_y = -15
 				self.jumped = True
+				self.jump_num += 1
 			if key[pygame.K_SPACE] == False:
 				self.jumped = False
 			if key[pygame.K_LEFT]:
@@ -100,7 +105,7 @@ class Player():
 				self.vel_y = speed_fall
 			dy += self.vel_y
 
-
+			
 			#столкновение с блоками
 			for tile in world.tile_list:
 				#столкновение по "х"
@@ -108,17 +113,20 @@ class Player():
 					dx = 0
 				#столкновение по "у"
 				if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.wid, self.heig):
+					self.jump_num = 0
 					#в прижке
 					if self.vel_y < 0:
 						dy = tile[1].bottom - self.rect.top
 						self.vel_y = 0
+						
 					#падает
 					elif self.vel_y >= 0:
 						dy = tile[1].top - self.rect.bottom
 						self.vel_y = 0
+						
 
 
-			#check for collision with lava
+			#столкновение с лавой
 			if pygame.sprite.spritecollide(self, lava_group, False):
 				dead = 1
 
@@ -140,6 +148,34 @@ class Player():
 		screen.blit(self.image, self.rect)
 
 		return dead
+	
+	def reset(self, x, y):
+		#списки с разными положениями персонажа во время анимации
+		self.hero_r = []
+		self.hero_l = []
+		#загрузка тайтлов анимации
+		for i in range(1, 8):
+			#оригинал
+			hero_l = pygame.image.load(f'img/slime{i}.png')
+			#изменение размеров
+			hero_l = pygame.transform.scale(hero_l, (100, 77))
+			#отражаю 
+			hero_r = pygame.transform.flip(hero_l, True, False)
+			self.hero_r.append(hero_r)
+			self.hero_l.append(hero_l)
+		#кадр в анимации (0 - стоит)
+		self.index = 0
+		self.coun = 0
+		
+		self.image = self.hero_r[self.index]
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.wid = self.image.get_width()
+		self.heig = self.image.get_height()
+		self.jumped = False
+		self.direction = 0
 
 
 
@@ -208,6 +244,35 @@ class LavaBlock(pygame.sprite.Sprite):
 		self.rect.y = y
 
 
+class Button():
+	def __init__(self, x, y, image):
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.push = False
+
+	def draw(self):
+		ret = False
+
+		#позиция мышки
+		pos = pygame.mouse.get_pos()
+
+		#нажатии мышки
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.push == False:
+				ret = True
+				self.push = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.push = False
+
+
+		#отрисовка кнопки
+		screen.blit(self.image, self.rect)
+
+		return ret
+
 world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
@@ -229,6 +294,7 @@ lava_group = pygame.sprite.Group()
 
 world = World(world_data)
 
+restart_button = Button(300, 300, restart_img)
 
 run = True
 while run:
@@ -244,6 +310,11 @@ while run:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
+
+	if dead == 1:
+		if restart_button.draw():
+				player.reset(100, screen_height - 130)
+				dead = 0
 
 	lava_group.draw(screen)
 	pygame.display.update()
